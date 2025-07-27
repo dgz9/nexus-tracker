18410f066448d165c9f5b3c7c627dc4d7cc9101d
David Gonzalez <dgonzalez1992@outlook.com>
2025-07-26 23:13:41 -0400

    add new cors url

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const corsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = [
       process.env.FRONTEND_URL,
+      process.env.FRONTEND_URL,
       'http://localhost:5173',
       'http://localhost:5174'
     ].filter(Boolean);
